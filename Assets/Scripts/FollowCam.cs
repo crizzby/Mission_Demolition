d4d47cf650b29ffa468dc0316107e8d237d2d557
crizzby <joel.wekker@gmx.de>
2024-07-15 23:11:38 +0200

    Fixed Camera smoothness

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -8,6 +8,7 @@ public class FollowCam : MonoBehaviour
 
     [Header("Inscribed")]
     public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero; //Vector2.zero is [0,0]
 
     [Header("Dynamic")]
     public float camZ;  //The desired Z pos of the Camera
@@ -19,16 +20,29 @@ public class FollowCam : MonoBehaviour
 
     void FixedUpdate()
     {
-        //A single line if statement doesnt require braces
-        if(POI == null) return;
+        Vector3 destination = Vector3.zero;
 
-        //Get the position of the poi
-        Vector3 destination = POI.transform.position;
+        if(POI != null)
+        {
+            Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+            if ((poiRigid != null) && poiRigid.IsSleeping())
+            {
+                POI = null;
+            }
+        }
+        if(POI != null)
+        {
+            destination = POI.transform.position;
+        }
+        //Limit the minimum values of destination.x & destination.y
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
         //Interpolate from the current Camera position toward destination
         destination = Vector3.Lerp(transform.position, destination, easing);
         //Forces destination.z to be camZ to keep the camera far enough away
         destination.z = camZ;
         //Set the camera to the destination
         transform.position = destination;
+        Camera.main.orthographicSize = destination.y +10;
     }
 }
